3f39ec20e42f6529fd425d37a502c40951134800
maggioniduffy <fausmaggioni5@gmail.com>
2023-10-04 22:43:38 -0300

    trying to fix sending emails in prod

--- src/services/nodeMailer.ts
+++ src/services/nodeMailer.ts
@@ -9,13 +9,26 @@ export async function sendMail(subject:string, toEmail:string, otpText:string) {
       user: "apikey",
       pass: process.env.API_KEY,
     },
-    secure: false,
-    tls: {
-      rejectUnauthorized: false
-    }
+    secure: true,
+    // tls: {
+    //   rejectUnauthorized: false
+    // }
   });
 
   console.log(subject,otpText)
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+  });
+
 
   var mailOptions = {
     from: process.env.NODEMAILER_EMAIL,
@@ -23,16 +36,19 @@ export async function sendMail(subject:string, toEmail:string, otpText:string) {
     subject: subject,
     text: otpText,
   };
-
-  transporter.sendMail(mailOptions, function (error:any, info:any) {
-    if (error) {
-      console.log('err in transporter ', error)
-      throw new Error(error);
-    } else {
-      console.log("Email Sent", mailOptions);
-      return true;
-    }
-  });
+  
+  await new Promise((resolve,reject) => {
+    transporter.sendMail(mailOptions, function (error:any, info:any) {
+      if (error) {
+        console.log('err in transporter ', error)
+        throw new Error(error);
+      } else {
+        console.log("Email Sent", mailOptions);
+        return true;
+      }
+    });
+  })
+  
 }
 
 // export async function send() {
