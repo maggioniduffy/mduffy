0c4ed48121888286964fbec26095193fe2ccccae
maggioniduffy <fausmaggioni5@gmail.com>
2023-10-04 22:54:10 -0300

    ready

--- src/services/nodeMailer.ts
+++ src/services/nodeMailer.ts
@@ -50,22 +50,3 @@ export async function sendMail(subject:string, toEmail:string, otpText:string) {
   })
   
 }
-
-// export async function send() {
-// sgMail.setApiKey(process.env.API_KEY+"")
-// const msg = {
-//   to: process.env.NODEMAILER_EMAIL+"", // Change to your recipient
-//   from: process.env.NODEMAILER_EMAIL+"", // Change to your verified sender
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// }
-// sgMail
-//   .send(msg)
-//   .then(() => {
-//     console.log('Email sent')
-//   })
-//   .catch((error) => {
-//     console.error(error)
-//   })
-// }
